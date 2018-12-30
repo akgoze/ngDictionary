c580b85f9455720287e8d6563087e7248ff9ada7
Fatih Akgöze <f.akgoze@gmail.com>
2018-12-30 12:08:30 +0300

    Added Word model.

---
+++ src/app/models/Word.ts
@@ -0,0 +1,9 @@
+export interface Word {
+  id?: string;
+  word_en?: string;
+  word_tr?: string;
+  word_type?: string;
+  word_sentece?: string;
+  word_added_by?: string;
+  created_date?: string;
+}
