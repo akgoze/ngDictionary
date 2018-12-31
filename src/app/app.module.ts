72ef96124af3f0e59f7a3294d8851510a2713856
Fatih Akgöze <f.akgoze@gmail.com>
2018-12-31 09:12:18 +0300

    Added FormModule.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
