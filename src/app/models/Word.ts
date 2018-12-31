7d9002171d13c10566df4f4d5b36f17f6f44b984
Fatih Akgöze <f.akgoze@gmail.com>
2018-12-31 09:11:38 +0300

    Updated Work model.

--- src/app/models/Word.ts
+++ src/app/models/Word.ts
@@ -1,9 +1,6 @@
 export interface Word {
-  id?: string;
   word_en?: string;
   word_tr?: string;
-  word_type?: string;
   word_sentece?: string;
-  word_added_by?: string;
-  created_date?: string;
+  created_date?: Date;
 }
