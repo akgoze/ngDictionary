82ce4b736fbb253e2dd41d61ace6e43d11825253
Fatih Akgöze <f.akgoze@gmail.com>
2019-01-20 23:34:55 +0300

    Added login method.

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from './../../services/auth.service';
+import { Router } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  email: string;
+  password: string;
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private flashMessage: FlashMessagesService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    this.authService.login(this.email, this.password).then(response => {
+      this.flashMessage.show('Logged in successfully', {
+        cssClass: 'alert-success'
+      });
+      this.router.navigate(['/']);
+    }).catch(err => {
+      this.flashMessage.show('Check your login information.', {
+        cssClass: 'alert-danger'
+      });
+    });
   }
 
 }
