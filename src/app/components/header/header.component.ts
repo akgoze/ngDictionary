c4a4b38d1185e21213252903004b260c3fccb00b
Fatih Akgöze <f.akgoze@gmail.com>
2019-01-20 23:34:07 +0300

    Updated menu status by user status.

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from './../../services/auth.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  loginStatus: boolean;
+  loggedInUser: string;
+  showRegister: boolean;
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private flashService: FlashMessagesService,
+    private router: Router) { }
 
   ngOnInit() {
+    this.authService.getAuth().subscribe(auth => {
+      if (auth) {
+        this.loginStatus = true;
+      } else {
+        this.loginStatus = false;
+        this.router.navigate(['/login']);
+      }
+    });
   }
 
+  signOut() {
+    this.authService.logout();
+    this.flashService.show('Logout successfully', {
+      cssClass: 'alert-success'
+    });
+  }
 }
