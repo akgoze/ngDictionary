532b307fd9b6787931f2fa67d538c51f0c41dc63
Fatih Akgöze <f.akgoze@gmail.com>
2019-01-20 23:28:45 +0300

    Added angular flash message package.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,8 @@ import { AngularFireStorageModule } from '@angular/fire/storage';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { environment } from './../environments/environment';
 
+import { FlashMessagesModule } from 'angular2-flash-messages';
+
   import {
     MatButtonModule,
     MatToolbarModule,
@@ -75,6 +77,7 @@ import { environment } from './../environments/environment';
     AngularFirestoreModule,
     AngularFireStorageModule,
     AngularFireAuthModule,
+    FlashMessagesModule.forRoot(),
     RouterModule.forRoot(
       appRoutes,
     ),
